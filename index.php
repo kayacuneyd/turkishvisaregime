c7b31a526b1b1ec2ca81585ef04a20de8e6ac123
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2025-11-10 09:27:38 +0000

    Meta-Beschreibung im HTML-Code aktualisiert

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $data = file_get_contents(__DIR__ . '/data/data.json');
     <link rel="stylesheet" href="assets/css/custom.css" />
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg" />
     <meta name="description" 
-    content="Search visa requirements, e-Visa eligibility, and stay limits for traveling to Türkiye" />
+    content="Search visa requirements, e-Visa eligibility, and stay limits for traveling to Türkiye..." />
   </head>
   <body class="text-slate-100 antialiased">
     <main class="relative px-4 sm:px-6 lg:px-8 py-12 md:py-16">
